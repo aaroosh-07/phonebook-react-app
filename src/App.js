29acafd4672e7120785bee74477e588a030ffcd4
Aaroosh Agarwal <aaroosh.2012@gmail.com>
2022-01-28 17:36:45 +0530

    adds function to fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", num: "99999999" }
-  ]);
+  const [persons, setPersons] = useState(null);
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleInput = (event) => {
     const temp = event.target.value;
@@ -28,6 +28,16 @@ const App = () => {
     }
   };
 
+  useEffect(()=>{
+    console.log('use effect in effect');
+    axios.get('http://localhost:3001/persons').then(response=>{
+      const data = response.data;
+      setPersons(data);
+      setIsLoading(false);
+      console.log(data);
+    });
+  },[]);
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -41,7 +51,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {isLoading && <p>loading......</p>}
+      {persons && persons.map((person) => (
         <p>
           {person.name} {person.num}
         </p>
