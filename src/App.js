928e0c013d0edefeb11e6ac377a94d6286194d3c
Aaroosh Agarwal <aaroosh.2012@gmail.com>
2022-01-28 13:50:35 +0530

    adds basic functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", num: "99999999" }
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNum, setNewNum] = useState("");
+
+  const handleInput = (event) => {
+    const temp = event.target.value;
+    console.log(event.target);
+    setNewName(temp);
+  };
+  const handleInputNum = (event) => {
+    const temp = event.target.value;
+    setNewNum(temp);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const t = { name: newName, num: newNum };
+    const found = persons.find((person) => person.name === t.name);
+    if (!found) {
+      setPersons(persons.concat(t));
+      setNewName("");
+    } else {
+      alert(`${newName} already exist in phonebook`);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleInput} />
+          number: <input value={newNum} onChange={handleInputNum} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {person.name} {person.num}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
